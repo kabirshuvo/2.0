c2fdb090bd109d51c1420897cf0b6e35b1bfde31
Kabir <kabirshuvo19@gmail.com>
2024-05-06 02:25:49 +0600

    single example for type guard

--- modules/src/module3/3_2_inheritanceinOOP.ts
+++ modules/src/module3/3_2_inheritanceinOOP.ts
@@ -1,40 +1,44 @@
-class Developer {
-  name: string;
-  age: number;
-  address: string;
+{
+  // inheritabce in OOP
 
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
+  class Developer {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+
+    playGames(numberOfHours: number) {
+      console.log(`${this.name} plays games for ${numberOfHours} hours.`);
+    }
   }
 
-  playGames(numberOfHours: number) {
-    console.log(`${this.name} plays games for ${numberOfHours} hours.`);
+  class GameDeveloper extends Developer {
+    favoriteGame: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      favoriteGame: string
+    ) {
+      super(name, age, address);
+      this.favoriteGame = favoriteGame;
+    }
+
+    developGame(gameName: string) {
+      console.log(`${this.name} develops ${gameName}.`);
+    }
   }
+
+  const developerOne = new Developer("Mr. X", 20, "Bangladesh");
+  const gameDeveloperOne = new GameDeveloper("Mr. Y", 25, "USA", "Fortnite");
+
+  developerOne.playGames(3); // Output: Mr. X plays games for 3 hours.
+  gameDeveloperOne.playGames(2); // Output: Mr. Y plays games for 2 hours.
+  gameDeveloperOne.developGame("Space Invaders"); // Output: Mr. Y develops Space Invaders.
 }
-
-class GameDeveloper extends Developer {
-  favoriteGame: string;
-
-  constructor(
-    name: string,
-    age: number,
-    address: string,
-    favoriteGame: string
-  ) {
-    super(name, age, address);
-    this.favoriteGame = favoriteGame;
-  }
-
-  developGame(gameName: string) {
-    console.log(`${this.name} develops ${gameName}.`);
-  }
-}
-
-const developerOne = new Developer("Mr. X", 20, "Bangladesh");
-const gameDeveloperOne = new GameDeveloper("Mr. Y", 25, "USA", "Fortnite");
-
-developerOne.playGames(3); // Output: Mr. X plays games for 3 hours.
-gameDeveloperOne.playGames(2); // Output: Mr. Y plays games for 2 hours.
-gameDeveloperOne.developGame("Space Invaders"); // Output: Mr. Y develops Space Invaders.
